efa88a5b4f6b394290a39d2e914dac35e35eba89
Onkar Dhillon <onkardhillon73@gmail.com>
2025-09-23 13:13:14 -0400

    Pushing updated project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,4 +19,25 @@ const config = {
   plugins: [heroui()],
 }
 
-module.exports = config;
+module.exports =  config;// {
+//   plugins: [
+//     heroui({
+//       prefix: "heroui", // prefix for themes variables
+//       addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+//       defaultTheme: "dark", // default theme from the themes object
+//       defaultExtendTheme: "dark", // default theme to extend on custom themes
+//       layout: {}, // common layout tokens (applied to all themes)
+//       themes: {
+//         light: {
+//           layout: {}, // light theme layout tokens
+//           colors: {}, // light theme colors
+//         },
+//         dark: {
+//           layout: {}, // dark theme layout tokens
+//           colors: {}, // dark theme colors
+//         },
+//         // ... custom themes
+//       },
+//     }),
+//   ],
+// };
