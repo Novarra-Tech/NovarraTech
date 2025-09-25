2e079d5efc0475182acf8f779e858d77ef6dc617
Onkar Dhillon <onkardhillon73@gmail.com>
2025-09-25 12:11:04 -0400

    I dont what i did.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,7 +49,7 @@ export const Navbar = () => {
   );*/
 
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky" shouldHideOnScroll >
+    <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
