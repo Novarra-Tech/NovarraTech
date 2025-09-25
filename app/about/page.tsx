7a13c0bddc9e2d4fce16d2e4662567debf8f96eb
Gabriel Kaloo <gkaloo17591@gmail.com>
2025-09-25 17:21:12 -0400

    All titles and descriptions updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,15 +25,21 @@ const team: Member[] = [
 
   { 
     name: "Sehajveer Dhillon", 
-    role: "Role", 
-    about: "Hi my name is Gabe and I like blah blah blah.", 
+    role: "Managing Partner, CIO",
+    about: "Sehaj is a senior Computer Science student with expertise in programming and cybersecurity. " +
+        "He has hands-on experience as a full stack developer at a tech startup that integrates real estate and AI, giving him valuable insight into building modern, innovative applications." + "In addition to his development work, Sehaj is an Audio Visual Technician at Adelphi University, where he helps monitor and maintain classroom technology across campus. " +
+        "His dual background in software development and AV systems allows him to approach challenges with both technical depth and practical problem-solving skills." +
+        "Sehaj’s mix of programming expertise, cybersecurity knowledge, and real-world AV support experience makes him a versatile team member dedicated to delivering technology solutions that are secure, reliable, and user-friendly.",
     img: "/images/sehaj.jpeg" 
   },
 
   { 
     name: "Onkar Dhillon", 
-    role: "Role", 
-    about: "Hi my name is Gabe and I like blah blah blah.", 
+    role: "Managing Partner, COO",
+    about: "Onkar is a senior Computer Science student with deep expertise in programming and cybersecurity. " +
+        "Skilled in multiple programming languages and experienced in research-driven projects, he brings a forward-thinking approach to technology solutions" + "He also leads a Help Desk team of IT service specialists within the Adelphi domain, where they monitor and assist over 100,000 users. " +
+        "Through this leadership, Onkar ensures that organizations receive reliable support, consistent service standards, and an exceptional customer experience" +
+        "By combining technical knowledge with hands-on leadership, Onkar helps clients achieve technology goals with efficiency, security, and confidence",
     img: "/images/image.png" 
   },
 ];
