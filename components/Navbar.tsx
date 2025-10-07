1ae321a5696e0d0ef79e46e5bc53ed388ee3441b
Onkar Dhillon <onkardhillon73@gmail.com>
2025-10-07 11:27:21 -0400

    Fixing Links using heroUI links instead.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,8 +12,6 @@ import {Button} from "@heroui/button";
 import {Kbd} from "@heroui/kbd";
 import {Link} from "@heroui/link";
 import {Input} from "@heroui/input";
-import {link as linkStyles} from "@heroui/theme";
-import NextLink from "next/link";
 import React, { useEffect, useRef } from 'react';
 import clsx from "clsx";
 
@@ -61,24 +59,23 @@ export const Navbar = () => {
                     className="sm:hidden"
                 />
                 <NavbarBrand as="li" className="gap-3 max-w-fit">
-                    <NextLink className="flex justify-start items-center gap-1" href="/">
+                    <Link className="flex justify-start items-center gap-1" href="/">
                         <Logo/>
                         <p className="font-bold text-inherit">Novarra</p>
-                    </NextLink>
+                    </Link>
                 </NavbarBrand>
                 <ul className="hidden lg:flex gap-4 justify-start ml-2">
                     {siteConfig.navItems.map((item) => (
                         <NavbarItem key={item.href}>
-                            <NextLink
+                            <Link
                                 className={clsx(
-                                    linkStyles({color: "foreground"}),
                                     "data-[active=true]:text-primary data-[active=true]:font-medium",
                                 )}
                                 color="foreground"
                                 href={item.href}
                             >
                                 {item.label}
-                            </NextLink>
+                            </Link>
                         </NavbarItem>
                     ))}
                 </ul>
@@ -97,9 +94,8 @@ export const Navbar = () => {
                 <div className="mx-4 mt-2 flex flex-col gap-2">
                     {siteConfig.navItems.map((item) => (
                         <NavbarItem key={item.href}>
-                            <NextLink
+                            <Link
                                 className={clsx(
-                                    linkStyles({color: "foreground"}),
                                     "data-[active=true]:text-primary data-[active=true]:font-medium",
                                 )}
                                 color="foreground"
@@ -107,7 +103,7 @@ export const Navbar = () => {
                                 onClick={() => setIsMenuOpen(false)}
                             >
                                 {item.label}
-                            </NextLink>
+                            </Link>
                         </NavbarItem>
                     ))}
                 </div>
