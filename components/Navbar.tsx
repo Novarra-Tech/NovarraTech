927bd1ce31085534d8ba50ac9d76937814219a30
Onkar Dhillon <onkardhillon73@gmail.com>
2025-10-07 11:13:56 -0400

    Fixing Mobile Menu.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -95,22 +95,19 @@ export const Navbar = () => {
             <NavbarMenu>
                 {/*searchInput*/}
                 <div className="mx-4 mt-2 flex flex-col gap-2">
-                    {siteConfig.navMenuItems.map((item, index) => (
-                        <NavbarMenuItem key={`${item}-${index}`}>
-                            <Link
-                                color={
-                                    index === 2
-                                        ? "primary"
-                                        : index === siteConfig.navMenuItems.length - 1
-                                            ? "danger"
-                                            : "foreground"
-                                }
-                                href="#"
-                                size="lg"
+                    {siteConfig.navItems.map((item) => (
+                        <NavbarItem key={item.href}>
+                            <NextLink
+                                className={clsx(
+                                    linkStyles({color: "foreground"}),
+                                    "data-[active=true]:text-primary data-[active=true]:font-medium",
+                                )}
+                                color="foreground"
+                                href={item.href}
                             >
                                 {item.label}
-                            </Link>
-                        </NavbarMenuItem>
+                            </NextLink>
+                        </NavbarItem>
                     ))}
                 </div>
             </NavbarMenu>
